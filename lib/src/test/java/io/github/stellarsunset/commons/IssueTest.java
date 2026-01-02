dcdbb544f12928c6450b58407b3df663a5a91889
Alex Cramer <stellarsunset@proton.me>
2026-01-02 12:27:27 -0500

    Unit test

--- lib/src/test/java/io/github/stellarsunset/commons/IssueTest.java
+++ lib/src/test/java/io/github/stellarsunset/commons/IssueTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class IssueTest {
 
     @Test
-    void testAsThrowable() {
+    void testAsThrowable_ExceptionThrown() {
         var exception = new IllegalArgumentException();
         var issue = Issue.exceptionThrown(exception);
 
@@ -17,5 +17,18 @@ class IssueTest {
                         "Should see the exception as suppressed"),
                 () -> assertEquals(exception, throwable.getCause(), "Should see exception in cause")
         );
+    }
+
+    @Test
+    void testAsThrowable_AllOf() {
+        var exception = new IllegalArgumentException();
+        var issue = Issue.allOf(
+                Issue.exceptionThrown(exception),
+                Issue.exceptionThrown(exception)
+        );
+
+        var throwable = issue.asException();
+        assertArrayEquals(new Throwable[]{exception, exception}, throwable.getSuppressed(),
+                "Should see the exception as suppressed");
     }
 }
